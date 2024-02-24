5751dd95cfa3a08dfafef277e54583256cda2dac
aziret <kubaaziret68@gmail.com>
2024-02-24 10:02:24 +0600

    debagugging

--- src/pages/addProduct/addproduct.jsx
+++ src/pages/addProduct/addproduct.jsx
@@ -1,111 +1,94 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Header from "../../components/header/header";
 import "./addproduct.css";
 import { useLocation } from "react-router-dom";
+import Context from "../../Contexts";
 
 function Addproduct() {
-    const [data, setdata] = useState({
-        url: "",
-        name: "",
-        price: 0,
-        descrip: "",
-    });
+  const { data2 } = useContext(Context);
+  const [data, setdata] = useState({
+    url: "",
+    name: "",
+    price: 0,
+    descrip: "",
+  });
+  const [editing, setEditing] = useState(false);
 
-    const [editing, setEditing] = useState(false);
-
-    const { search } = useLocation();
-    const param = new URLSearchParams(search);
-    const id = param.get("Editid");
-    useEffect(() => {
-        if (id !== null) {
-            let items = JSON.parse(localStorage.getItem("products")) || [];
-            const editproducts = items.find((el) => el.id == id);
-            if (editproducts) {
-                setdata(editproducts);
-                setEditing(true);
-                console.log(data);
-            }
-        }
-    }, [id]);
-
-    function submitHandler(e) {
-        e.preventDefault();
-        let product = {
-            ...data,
-            id: new Date().getTime(),
-            date: new Date().getTime(),
-        };
-
-        const products = JSON.parse(localStorage.getItem("products")) || [];
-        if (editing) {
-            let updateProducts = products.map((el) => {
-                if (el.id == product.id) {
-                    return product;
-                } else {
-                    return el;
-                }
-            });
-
-            localStorage.setItem("products", JSON.stringify(updateProducts));
-        } else {
-            products.push(product);
-            localStorage.setItem("products", JSON.stringify(products));
-        }
-
-        setdata({
-            url: "",
-            name: "",
-            price: 0,
-            descrip: "",
-        });
+  const { search } = useLocation();
+  const param = new URLSearchParams(search);
+  const id = param.get("Editid");
+  if (id !== null) {
+    const editproducts = data2.find((el) => el.id == id);
+    if (editproducts) {
+      setdata(editproducts);
+      setEditing(true);
     }
-    return (
-        <>
-            <div className="addproduct">
-                <Header />
-                <div className="container">
-                    <form onSubmit={submitHandler} className="form">
-                        <input
-                            placeholder="image"
-                            value={data.url}
-                            className="form__input"
-                            onChange={(e) =>
-                                setdata({ ...data, url: e.target.value })
-                            }
-                        />
+  }
+  function submitHandler(e) {
+    e.preventDefault();
+    let product = {
+      ...data,
+      id: new Date().getTime(),
+      date: new Date().getTime(),
+    };
+    if (editing) {
+      let updateProducts = data2.map((el) => {
+        if (el.id == product.id) {
+          return product;
+        } else {
+          return el;
+        }
+      });
+      data2.push(updateProducts);
+    } else {
+      data2.push(product);
+    }
+    setdata({
+      url: "",
+      name: "",
+      price: 0,
+      descrip: "",
+    });
+  }
+  return (
+    <>
+      <div className="addproduct">
+        <Header />
+        <div className="container">
+          <form onSubmit={submitHandler} className="form">
+            <input
+              placeholder="image"
+              value={data.url}
+              className="form__input"
+              onChange={(e) => setdata({ ...data, url: e.target.value })}
+            />
 
-                        <input
-                            placeholder="Name"
-                            value={data.name}
-                            onChange={(e) =>
-                                setdata({ ...data, name: e.target.value })
-                            }
-                            className="form__input"
-                        />
-                        <input
-                            placeholder="price"
-                            value={data.price}
-                            onChange={(e) =>
-                                setdata({ ...data, price: e.target.value })
-                            }
-                            className="form__input"
-                        />
-                        <input
-                            placeholder="description"
-                            value={data.descrip}
-                            onChange={(e) =>
-                                setdata({ ...data, descrip: e.target.value })
-                            }
-                            className="form__input"
-                        />
-                        <button type="submit" className="btn__input">
-                            {editing ? "Edit" : "SUBMIT"}
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </>
-    );
+            <input
+              placeholder="Name"
+              value={data.name}
+              onChange={(e) => setdata({ ...data, name: e.target.value })}
+              className="form__input"
+            />
+            <input
+              placeholder="price"
+              value={data.price}
+              onChange={(e) => setdata({ ...data, price: e.target.value })}
+              className="form__input"
+            />
+            <input
+              placeholder="description"
+              value={data.descrip}
+              onChange={(e) => setdata({ ...data, descrip: e.target.value })}
+              className="form__input"
+            />
+            <button type="submit" className="btn__input">
+              {editing ? "Edit" : "SUBMIT"}
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Addproduct;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,45 +1,42 @@
 import Card from "../../components/card/card";
 import Header from "../../components/header/header";
 import Hero from "../../components/hero/hero";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import "./home.css";
 import { useNavigate } from "react-router-dom";
+import Context from "../../Contexts";
 function Home() {
-    const [data, setData] = useState([]);
+  //   const { data } = useContext();
+  const { data2 } = useContext(Context);
+  // const [data, setData] = useState([]);
 
-    const nav = useNavigate();
+  const nav = useNavigate();
 
-    useEffect(() => {
-        const localStorageData =
-            JSON.parse(localStorage.getItem("products")) || [];
-        setData(localStorageData);
-    }, []);
+  function CardclickHandler(id) {
+    nav(`/product/${id}`);
+  }
 
-    function CardclickHandler(id) {
-        nav(`/product/${id}`);
-    }
+  return (
+    <>
+      <Header />
+      <Hero />
+      <h1 className="home__title">HOT DEALS</h1>
 
-    return (
-        <>
-            <Header />
-            <Hero />
-            <h1 className="home__title">HOT DEALS</h1>
-
-            <div className="cards">
-                {data.slice(0, 3).map((product) => (
-                    <>
-                        <div
-                            className="cards"
-                            onClick={() => CardclickHandler(product.id)}
-                            key={product.id}
-                        >
-                            <Card products={product} />
-                        </div>
-                    </>
-                ))}
+      <div className="cards">
+        {data2?.map((product) => (
+          <>
+            <div
+              className="cards"
+              onClick={() => CardclickHandler(product.id)}
+              key={product.id}
+            >
+              <Card products={product} />
             </div>
-        </>
-    );
+          </>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Home;
