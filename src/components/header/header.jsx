70a7e88a650a19abcf587dba9a6ce721c275bc4b
aziret <kubaaziret68@gmail.com>
2024-02-24 09:15:05 +0600

    lkjhgfdfgh

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -14,7 +14,7 @@ function Header() {
               </Link>
               <nav className="header__nav">
                 <Link to="/Allcars/:id" className="nav__link">
-                  ALLCARS AZIRET sultan
+                  ALLCARS AZIRET sultan lol
                 </Link>
                 <Link to="/" className="nav__link">
                   HOME
