4049100d0c26f6e6f83fb8aabbe69744269353cc
aziret <kubaaziret68@gmail.com>
2024-02-24 09:08:00 +0600

    header_test_git

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -14,7 +14,7 @@ function Header() {
               </Link>
               <nav className="header__nav">
                 <Link to="/Allcars/:id" className="nav__link">
-                  ALLCARS sultan
+                  ALLCARS AZIRET
                 </Link>
                 <Link to="/" className="nav__link">
                   HOME
